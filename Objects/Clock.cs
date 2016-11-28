6f02872cf9637e406a98601da49bf89141027916
Anne Belka and Levi Bibo <anne+levi@>
2016-11-28 14:07:06 -0800

    add funcitonality to find shortest length between clock hands.

--- Objects/Clock.cs
+++ Objects/Clock.cs
@@ -9,20 +9,34 @@ namespace ClockDegrees.Objects
 		private int _minute;
 		private int _hourDegreesClockwise;
 		private int _hourDegreesCounterClockwise;
-		private int _minuteDegrees;
+		private int _minuteDegreesClockwise;
+		private int _minuteDegreesCounterClockwise;
+		private static List<Clock> _allClocks = new List<Clock>{};
+
+		public static void DeleteAll()
+		{
+			_allClocks.Clear();
+		}
 
 		public Clock(int hour, int minute)
 		{
+			_allClocks.Add(this);
 			_hour = hour;
 			_minute = minute;
-			_minuteDegrees = _minute * 6;
+			_minuteDegreesClockwise = _minute * 6;
+			_minuteDegreesCounterClockwise = Math.Abs(_minuteDegreesClockwise - 360);
 			_hourDegreesClockwise = (30 * _hour) + (_minute / 2);
 			_hourDegreesCounterClockwise = Math.Abs(_hourDegreesClockwise - 360);
 		}
 
-		public int GetMinuteDegrees()
+		public int GetMinuteDegreesClockwise()
 		{
-			return _minuteDegrees;
+			return _minuteDegreesClockwise;
+		}
+
+		public int GetMinuteDegreesCounterClockwise()
+		{
+			return _minuteDegreesCounterClockwise;
 		}
 
 		public int GetHourDegreesClockwise()
@@ -35,13 +49,27 @@ namespace ClockDegrees.Objects
 			return _hourDegreesCounterClockwise;
 		}
 
-		public int FindShortestDistance()
-		{
-			return _hour;
-		}
 		public int FindDistanceClockwise()
 		{
-			return Math.Abs(_hourDegreesClockwise-_minuteDegrees);
+			return Math.Abs(_hourDegreesClockwise + _minuteDegreesCounterClockwise) % 360;
+		}
+
+		public int FindDistanceCounterClockwise()
+		{
+			return Math.Abs(_hourDegreesCounterClockwise + _minuteDegreesClockwise) % 360;
+		}
+		public int FindShortestDistance()
+		{
+			int counterClockwise = this.FindDistanceCounterClockwise();
+			int clockwise = this.FindDistanceClockwise();
+			if (counterClockwise <= clockwise)
+			{
+				return counterClockwise;
+			}
+			else
+			{
+				return clockwise;
+			}
 		}
 	}
 }
--- Tests/TestClock.cs
+++ Tests/TestClock.cs
@@ -1,13 +1,20 @@
 using Xunit;
+using System;
 namespace ClockDegrees.Objects
 {
-  public class ClockTest
+  public class ClockTest : IDisposable
   {
     [Fact]
-    public void GetMinuteDegrees_FindMinuteDegrees_36()
+    public void GetMinuteDegreesClockwise_FindMinuteDegrees_30()
     {
-      Clock newClock = new Clock(1, 6);
-      Assert.Equal(36, newClock.GetMinuteDegrees());
+      Clock newClock = new Clock(0, 5);
+      Assert.Equal(30, newClock.GetMinuteDegreesClockwise());
+    }
+    [Fact]
+    public void GetMinuteDegreesCounterClockwise_FindMinuteDegrees_30()
+    {
+      Clock newClock = new Clock(0, 55);
+      Assert.Equal(30, newClock.GetMinuteDegreesCounterClockwise());
     }
     [Fact]
     public void GetHourDegreesClockwise_FindHourDegrees_30()
@@ -22,10 +29,26 @@ namespace ClockDegrees.Objects
       Assert.Equal(30, newClock.GetHourDegreesCounterClockwise());
     }
     [Fact]
-    public void GetDistanceClockwise_FindDinstanceClockwise_3()
+    public void GetDistanceClockwise_FindDinstanceClockwise_308()
     {
-      Clock newClock = new Clock (1,6);
-      Assert.Equal(3, newClock.FindDistanceClockwise());
+      Clock newClock = new Clock (11, 4);
+      Assert.Equal(308, newClock.FindDistanceClockwise());
+    }
+    [Fact]
+    public void GetDistanceCounterClockwise_FindDinstanceCounterClockwise_52()
+    {
+      Clock newClock = new Clock (11, 4);
+      Assert.Equal(52, newClock.FindDistanceCounterClockwise());
+    }
+    [Fact]
+    public void GetShortestDistance_FindShortestDistance_52()
+    {
+      Clock newClock = new Clock (1, 56);
+      Assert.Equal(82, newClock.FindShortestDistance());
+    }
+    public void Dispose()
+    {
+      Clock.DeleteAll();
     }
   }
 }
